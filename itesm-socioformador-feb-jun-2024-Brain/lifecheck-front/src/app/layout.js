abb0fb15d1e705482c1bcbc1b5837ef879608e47
Gabriel Ruiz Miller <110938299+GabrielRuizMiller@users.noreply.github.com>
2024-04-22 17:56:49 -0600

    Proyecto Completo
    Proyecto

---
+++ itesm-socioformador-feb-jun-2024-Brain/lifecheck-front/src/app/layout.js
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "../css/style.css";
+import Header from "../components/Header";
+import Header2 from "../components/Header2";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "LifeCheck",
+  description: "Quality of life assessment tool",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background antialiased",
+          inter.className
+        )}
+      >
+        <UserProvider>
+          <Header />
+          <div className="pt-20">{children}</div>
+        </UserProvider>
+      </body>
+    </html>
+  );
+}
